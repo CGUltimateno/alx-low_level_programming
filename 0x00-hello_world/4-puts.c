5274fa5139a969b7e20aab84116745afe78a9454
CGUltimateno <Mohameddeaf65@gmail.com>
2023-03-16 08:40:04 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+/**
+ * A program that prints that following line.
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
