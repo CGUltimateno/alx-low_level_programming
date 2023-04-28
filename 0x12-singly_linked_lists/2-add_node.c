0605b9d3b040896bc2628cfa646ba123b2a06899
CGU <Mohameddeaf65@gmail.com>
2023-04-28 09:57:11 +0300

    yes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "lists.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s : s is a character
+ * Return: value is i
+ */
 
+int _strlen(const char* s) {
+	int i = 0;
+	while (s[i] != '\0') { i++; }
+	return(i);
+}
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
