dae3bf4fed98c86cf8df2e8c847c0e0de698db52
CGU <Mohameddeaf65@gmail.com>
2023-04-28 10:50:04 +0300

    ay

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,18 +11,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	int x = 0;
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		x++;
 	}
 
-	return (s);
+	return (x);
 }
 
