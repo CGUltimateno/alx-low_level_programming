37a0386df2e754a837ef167bbef233ef6591b4fd
CGU <Mohameddeaf65@gmail.com>
2023-04-28 11:09:10 +0300

    yesyesy

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,12 @@
  */
 void free_list(list_t *head)
 {
-	if (head) 
+	list_t tmp;
+	while(head) 
 	{
-		free_list(head->next);
-
-		if (head->str) 
-			free(head->str);
-
+		tmp = head->next;
+		free(head->str);
 		free(head);
+		tmp = head;
 	}
 }
