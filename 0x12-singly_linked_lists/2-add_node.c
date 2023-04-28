20a3383825566423b48acff856386a0b0e05bd24
CGU <Mohameddeaf65@gmail.com>
2023-04-28 10:33:25 +0300

    a

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 #include "lists.h"
-
 /**
  * add_node - adds a new node
  * @head: double pointer
@@ -10,9 +8,9 @@
  *
  * Return: the address of the new element, or NULL if it fails
  */
-list_t* add_node(list_t** head, const char* str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t* list;
+	list_t *list;
 	unsigned int strlen = 0;
 
 	while (str[strlen])
