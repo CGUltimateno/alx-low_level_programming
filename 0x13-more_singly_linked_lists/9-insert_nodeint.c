bd4f8cb9f122bbf008adab9ece27490515612a5a
CGU <Mohameddeaf65@gmail.com>
2023-05-01 17:26:05 +0300

    adada

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,36 +9,36 @@
  *
  * Return: pointer to the new node, or NULL
  */
-listint_t* insert_nodeint_at_index(listint_t** head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t* new;
-	listint_t* temp = *head;
+	listint_t *node;
+	listint_t *tmp = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	node = malloc(sizeof(listint_t));
+	if (!node || !head)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	node->n = n;
+	node->next = NULL;
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		node->next = *head;
+		*head = node;
+		return (node);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; tmp && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			node->next = tmp->next;
+			tmp->next = node;
+			return (node);
 		}
 		else
-			temp = temp->next;
+			tmp = tmp->next;
 	}
 
 	return (NULL);
