59a3d771e6e5decb5f647d759f8f36eda0b2fdc1
CGUltimateno <Mohameddeaf65@gmail.com>
2023-06-11 00:40:36 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, j, result;
+	int number, x, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
@@ -21,21 +21,21 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num = atoi(argv[1]);
+	number = atoi(argv[1]);
 	result = 0;
 
-	if (num < 0)
+	if (number < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	for (j = 0; j < 5 && num >= 0; j++)
+	for (x = 0; x < 5 && number >= 0; x++)
 	{
-		while (num >= coins[j])
+		while (number >= coins[x])
 		{
 			result++;
-			num -= coins[j];
+			number -= coins[x];
 		}
 	}
 
