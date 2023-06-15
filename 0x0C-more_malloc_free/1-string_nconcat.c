692c4a3f59b1c54387eab0b26163b26350d8b4fc
CGUltimateno <Mohameddeaf65@gmail.com>
2023-06-16 02:25:15 +0300

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *string;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *s;
+	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	while (s1 && s1[length1])
+		length1++;
+	while (s2 && s2[length2])
+		length2++;
 
-	if (n < len2)
-		string = malloc(sizeof(char) * (len1 + n + 1));
+	if (n < length2)
+		s = malloc(sizeof(char) * (length1 + n + 1));
 	else
-		string = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (length1 + length2 + 1));
 
-	if (!string)
+	if (!s)
 		return (NULL);
 
-	while (i < len1)
+	while (i < length1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
 
-	while (n < len2 && i < (len1 + n))
-		string[i++] = s2[j++];
+	while (n < length2 && i < (length1 + n))
+		s[i++] = s2[j++];
 
-	while (n >= len2 && i < (len1 + len2))
-		string[i++] = s2[j++];
+	while (n >= length2 && i < (length1 + length2))
+		s[i++] = s2[j++];
 
-	string[i] = '\0';
+	s[i] = '\0';
 
-	return (string);
+	return (s);
 }
