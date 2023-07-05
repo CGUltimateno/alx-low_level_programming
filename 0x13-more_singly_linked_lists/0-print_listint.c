8c19e3e70cb33599db63ced9cba1ad0e383d3011
CGUltimateno <Mohameddeaf65@gmail.com>
2023-07-05 13:31:45 +0300

    hmm

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t x = 0;
+    size_t count = 0;
 
     while (h != NULL)
     {
         printf("%d\n", h->n);
         h = h->next;
-        x++;
+        count++;
     }
-    return (x);
+    return (count);
 }
