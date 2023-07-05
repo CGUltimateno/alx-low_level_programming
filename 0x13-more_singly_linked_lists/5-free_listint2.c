c3f96736124b02a2e1e9d3aefbcc7603a02d252d
CGUltimateno <Mohameddeaf65@gmail.com>
2023-07-05 12:50:05 +0300

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	if (!head || !*head)
+		return;
+
+	free_listint2(&(*head)->next);
+	free(*head);
+	*head = NULL;
+}
+
+// By CGUltimateno
