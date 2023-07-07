0245a960b740b13c1b2d09573d940f3acfe6609e
CGUltimateno <Mohameddeaf65@gmail.com>
2023-07-07 14:40:32 +0300

    5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index > 63)
-        return (-1);
-
-    *n = (~(1UL << index) & *n);
+    unsigned long int x;
+    if(index > (sizeof(unsigned long int) * 8 -1))
+        return(-1);
+    x = ~(1 << index);
+    *n = *n & x;
     return (1);
 }
