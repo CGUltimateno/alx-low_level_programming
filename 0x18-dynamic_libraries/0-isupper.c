0fdb173ea7783efc1930137c8e8798879029ccbc
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-22 20:36:09 +0300

    first stuff

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if sucessful
+ */
+
+int _isupper(int c){
+    if (c >= 65 && c <= 95)
+        return (1);
+    else
+        return (0);
+}
