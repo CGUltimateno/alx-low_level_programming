11c861b2ef709aed392c63ffebb08a73d8eebd45
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-09 00:32:45 -0700

    yes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,17 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *y;
 unsigned long int i;
-
 y = malloc(sizeof(hash_table_t));
 if (y == NULL)
-    return (NULL);
-
+return (NULL);
 y->size = size;
 y->array = malloc(sizeof(hash_node_t *) * size);
 if (y->array == NULL)
-    return (NULL);
+return (NULL);
 for (i = 0; i < size; i++)
-    y->array[i] = NULL;
-
+y->array[i] = NULL;
 return (y);
 }
