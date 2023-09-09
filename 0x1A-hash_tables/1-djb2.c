0fb739d61dd26bedfe320e4b5483e875e71edca3
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-09 10:16:51 +0300

    1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,8 +3,7 @@
  * hash_djb2 - implementation of the djb2 algorithm
  */
 
-unsigned long int hash_djb2(const unsigned char *str)
-{
+unsigned long int hash_djb2(const unsigned char *str) {
     unsigned long int hash = 5381;
     int c;
     while ((c = *str++))
