5382a29a926566237b8c4d5ecdf289e066aac3f2
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-09 10:45:45 +0300

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,20 +4,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+    hash_node_t *currentNode;
     unsigned long int index;
-    hash_node_t *node;
 
-    if (ht == NULL || key == NULL)
+    if (ht == NULL || key == NULL || *key == '\0')
         return (NULL);
 
     index = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[index];
+    if (index >= ht->size)
+        return (NULL);
 
-    while (node != NULL)
-    {
-        if (strcmp(node->key, key) == 0)
-            return (node->value);
-        node = node->next;
-    }
-    return (NULL);
+    currentNode = ht->array[index];
+    while (currentNode && strcmp(currentNode->key, key) != 0)
+        currentNode = currentNode->next;
+
+    return ((currentNode == NULL) ? NULL : currentNode->value);
 }
