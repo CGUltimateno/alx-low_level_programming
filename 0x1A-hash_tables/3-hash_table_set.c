7f220f7f7316d55a46728dcf398338ec628cd1f1
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-10 23:58:56 -0700

    yes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,39 +9,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *newNode;
-    char *valueCopy;
-    unsigned long int index, i;
+hash_node_t *newNode;
+char *valueCopy;
+unsigned long int index, i;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-        return (0);
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+return (0);
 
-    valueCopy = strdup(value);
-    if (valueCopy == NULL)
-        return (0);
+valueCopy = strdup(value);
+if (valueCopy == NULL)
+return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    for (i = index; ht->array[i]; i++) {
-        if (strcmp(ht->array[i]->key, key) == 0) {
-            free(ht->array[i]->value);
-            ht->array[i]->value = valueCopy;
-            return (1);
-        }
-    }
+index = key_index((const unsigned char *)key, ht->size);
+for (i = index; ht->array[i]; i++)
+{
+if (strcmp(ht->array[i]->key, key) == 0)
+{
+free(ht->array[i]->value);
+ht->array[i]->value = valueCopy;
+return (1);
+}
+}
 
-    newNode = malloc(sizeof(hash_node_t));
-    if (newNode == NULL) {
-        free(valueCopy);
-        return (0);
-    }
-    newNode->key = strdup(key);
-    if (newNode->key == NULL) {
-        free(newNode);
-        return (0);
-    }
-    newNode->value = valueCopy;
-    newNode->next = ht->array[index];
-    ht->array[index] = newNode;
+newNode = malloc(sizeof(hash_node_t));
+if (newNode == NULL)
+{
+free(valueCopy);
+return (0);
+}
+newNode->key = strdup(key);
+if (newNode->key == NULL)
+{
+free(newNode);
+return (0);
+}
+newNode->value = valueCopy;
+newNode->next = ht->array[index];
+ht->array[index] = newNode;
 
-    return (1);
+return (1);
 }
