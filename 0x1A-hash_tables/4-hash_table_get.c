09f3ee28a41089a8ed0b5bbf2b029fa461ab33c5
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-11 10:38:08 +0300

    4 final

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,25 +8,25 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index = 0;
-    hash_node_t *node = NULL;
-    hash_node_t *tmp = NULL;
+unsigned long int index = 0;
+hash_node_t *node = NULL;
+hash_node_t *tmp = NULL;
 
-    if (ht == NULL || key == NULL || !ht->array)
-        return (NULL);
+if (ht == NULL || key == NULL || !ht->array)
+return (NULL);
 
-    index = key_index((unsigned char *)key, ht->size);
+index = key_index((unsigned char *)key, ht->size);
 
-    node = ht->array[index];
-    if (node == NULL)
-        return (NULL);
+node = ht->array[index];
+if (node == NULL)
+return (NULL);
 
-    if (strcmp(node->key, key) == 0)
-        return (node->value);
+if (strcmp(node->key, key) == 0)
+return (node->value);
 
-    for(tmp = node; tmp; tmp = tmp->next)
-        if (strcmp(tmp->key, key) == 0)
-            return (tmp->value);
+for(tmp = node; tmp; tmp = tmp->next)
+if (strcmp(tmp->key, key) == 0)
+return (tmp->value);
 
-    return (NULL);
+return (NULL);
 }
