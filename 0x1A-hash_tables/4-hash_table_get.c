86610b48f7460bf36ebdf4c30a8f82fa3c15f053
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-11 10:36:06 +0300

    4 2.0

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,20 +8,23 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
+    unsigned long int index = 0;
     hash_node_t *node = NULL;
     hash_node_t *tmp = NULL;
 
     if (ht == NULL || key == NULL || !ht->array)
         return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
+    index = key_index((unsigned char *)key, ht->size);
+
     node = ht->array[index];
     if (node == NULL)
         return (NULL);
+
     if (strcmp(node->key, key) == 0)
         return (node->value);
-    for(tmp = ht->array; tmp != NULL; tmp = tmp->next)
+
+    for(tmp = node; tmp; tmp = tmp->next)
         if (strcmp(tmp->key, key) == 0)
             return (tmp->value);
 
