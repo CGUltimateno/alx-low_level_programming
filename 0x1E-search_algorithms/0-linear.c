d5654addb3b31f3486e00a11ff522df5352f5caf
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-14 19:31:03 +0200

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,10 +1,10 @@
 #include "search_algos.h"
 /**
-* linear_search - searches for a value in an array of integers using the
- * Linear search algorithm
+* linear_search - searches for a value in an array using linear search
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
- * @value: value to search for in the array
+ * @value: value to search for
+ *
  * Return: first index where value is located, or -1 if it fails
 */
 
