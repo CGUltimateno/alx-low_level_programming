a6c80890a49faf23a47e38957d02e7ad4ce2770b
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-14 19:25:53 +0200

    h

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,14 +12,14 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t index = 0;
 
-if (array == NULL)
-    return (-1);
+if(array == NULL)
+return (-1);
 
 while (index < size)
 {
 printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-if (array[index] == value)
-    return (index);
+if(array[index] == value)
+return (index);
 
 index++;
 }
