99c9334db737917aad0a29f47919559c600f3cfe
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-14 20:00:11 +0200

    103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -9,22 +9,22 @@
  */
 int _binary_search(int *array, int low, int high, int value)
 {
-    int mid, index;
-    while (low <= high)
+int mid, index;
+while (low <= high)
     {
-        printf("Searching in array: ");
-        for (index = low; index < high; index++)
-            printf("%d, ", array[index]);
-        printf("%d\n", array[index]);
-        mid = (low + high) / 2;
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return (-1);
+printf("Searching in array: ");
+for (index = low; index < high; index++)
+printf("%d, ", array[index]);
+printf("%d\n", array[index]);
+mid = (low + high) / 2;
+if (array[mid] == value)
+return (mid);
+else if (array[mid] < value)
+low = mid + 1;
+else
+high = mid - 1;
+}
+return (-1);
 }
 
 /**
@@ -37,21 +37,20 @@ int _binary_search(int *array, int low, int high, int value)
  */
 int exponential_search(int *array, size_t size, int value)
 {
-    size_t i = 0, bound = 1;
-    if(!array)
-        return (-1);
-    while (bound < size)
-    {
-        printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
-        if (array[bound] >= value)
-            break;
-        i = bound;
-        bound *= 2;
-    }
-    printf("Value found between indexes [%lu] and [%lu]\n", bound / 2,
-            (bound > size - 1 ? size - 1 : bound));
-
-    i = bound / 2;
-    bound = (bound > size - 1 ? size - 1 : bound);
-    return (_binary_search(array, i, bound, value));
+size_t i = 0, bound = 1;
+if(!array)
+return (-1);
+while (bound < size)
+{
+printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+if (array[bound] >= value)
+break;
+i = bound;
+bound *= 2;
+}
+printf("Value found between indexes [%lu] and [%lu]\n", bound / 2,
+(bound > size - 1 ? size - 1 : bound));
+i = bound / 2;
+bound = (bound > size - 1 ? size - 1 : bound);
+return (_binary_search(array, i, bound, value));
 }
